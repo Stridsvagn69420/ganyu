f3816c44e9dd62e6b5b636619121efbd9ce8678e
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-06-06 18:31:54 +0200

    Fix yay updating

--- systemupdate/pacman.go
+++ systemupdate/pacman.go
@@ -8,13 +8,13 @@ func pacman(root *bool) {
 	utils.RunShell(*root, "pacman", "--noconfirm", "-Syu")
 }
 
-func yay(root *bool) {
-	utils.RunShell(*root, "yay", "--noconfirm", "--answerclean", "None", "--answerdiff", "None", "-Syu")
+func yay() {
+	utils.RunShell(false, "yay", "--noconfirm", "--answerclean", "None", "--answerdiff", "None", "-Syu")
 }
 
 func Arch(root bool) {
 	if utils.CommandExists("yay") {
-		yay(&root)
+		yay()
 	} else {
 		pacman(&root)
 	}
